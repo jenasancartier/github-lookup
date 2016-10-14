b206b73efe8bc5e81b71ed42445e1981c76a0eed
Jena SanCartier <jenasancartier@gmail.com>
2016-10-14 14:44:31 -0700

    add octocat mascot to page and begin styling results section

--- build/js/app.js
+++ build/js/app.js
@@ -27,18 +27,16 @@ var apiKey = require('./../.env').apiKey;
 var Repos = require("./../js/lookup.js").reposModule;
 var username;
 
-// var showUserName = function(userName){
-//   $("#showName").text(username + "'s repos:");
-// };
-
 var showRepo = function(repoName, details){
-  $("#showRepo").append("<li>" + repoName + " is an app about " + details + ".</li>");
+  $("#showRepo").append("<li>" + "<b>" + repoName + "</b>" + "<br>" + details + ".</li>");
 };
 
 $(document).ready(function(){
   var currentReposObject = new Repos();
   $("#githubSubmit").click(function(event){
     event.preventDefault();
+    $("#octocat").hide();
+    $("#results").show();
     $("#showRepo").empty();
     username = $("#githubName").val();
     $("#githubName").val("");
